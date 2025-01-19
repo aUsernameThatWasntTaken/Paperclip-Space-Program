ea030ed48f5cfacc54900324d826c58cdf309be3
aUsernameThatWasntTaken <maxymilianbyzdra@gmail.com>
2025-01-19 20:59:37 +0000

    Something to do with the GUI

--- runGame.py
+++ runGame.py
@@ -6,6 +6,10 @@ largeLabelFont = ("Segoe UI", 18)
 
 screen = tkinter.Tk()
 screen.title("PaperClip Space Program")
+
+screen.columnconfigure(0, weight=2)
+screen.columnconfigure(1, weight=1)
+screen.columnconfigure(2, weight=0)
 #Defines a frame for the tutorial text, next, and skip tutorial buttons.
 if game.saveData.tutorialProgress != -1:
     tutorial = tkinter.Frame(screen)
@@ -39,8 +43,8 @@ class ColonyManagement:
         self.buildMenu = tkinter.Label(self.frame, text="Colony build menu") #Change this later too.
         self.buildMenu.grid(row=1, column=1)
     
-    def grid(self, row: int, column: int, rowSpan = 1, columnSpan = 1):
-        self.frame.grid(row=row, column=column, rowspan=rowSpan, columnspan=columnSpan)
+    def grid(self, row: int, column: int, rowSpan = 1, columnSpan = 1, sticky: str = ""):
+        self.frame.grid(row=row, column=column, rowspan=rowSpan, columnspan=columnSpan, sticky=sticky)
 
 class ColonyResourceDashboard:
     def __init__(self, targetScreen):
@@ -60,20 +64,38 @@ class ColonyResourceDashboard:
         self.bodySelector = tkinter.OptionMenu(self.frame, self.selectedBody, *[body.displayedName for body in game.celestialBodies])
         self.bodySelector.pack()
 
-    def grid(self, row: int, column: int):
-        self.frame.grid(row=row,column=column)
+    def grid(self, row: int, column: int, sticky: str = ""):
+        self.frame.grid(row=row,column=column, sticky=sticky)
+
+class ResearchMenu:
+    def __init__(self, targetFrame):
+        self.frame = tkinter.Frame(targetFrame, highlightbackground="black", highlightthickness=2)
+
+        self.label = tkinter.Label(self.frame, text="Research Menu", font=largeLabelFont)
+        self.label.grid(row=0,column=0)
+
+        self.researcherCounter = tkinter.StringVar(value="Resaecher count goes heer.")
+        self.researcherCounterLabel = tkinter.Label(self.frame, textvariable=self.researcherCounter)
+        self.researcherCounterLabel.grid(row=1, column=0)
+
+        self.researchSpeedVar = tkinter.StringVar(value="Reaeaearch spead gooes hear.")
+        self.researchSpeedLabel = tkinter.Label(self.frame, textvariable=self.researchSpeedVar)
+        self.researchSpeedLabel.grid(row=1,column=1)
+
+    def grid(self, row: int, column: int, rowSpan = 1, columnSpan = 1, sticky: str = ""):
+        self.frame.grid(row=row, column=column, rowspan=rowSpan, columnspan=columnSpan, sticky=sticky)
 
 colonyManagement = ColonyManagement(screen)  #A 2 by 2 grid.
-colonyManagement.grid(row=1, column=0)
+colonyManagement.grid(row=1, column=0, sticky="news", rowSpan=2)
 
 colonyResourceDashboard = ColonyResourceDashboard(screen)
-colonyResourceDashboard.grid(row=1,column=1)
+colonyResourceDashboard.grid(row=1,column=1, sticky = "new")
 
 drawingBoard = tkinter.Label(screen, text="Drawing Board", font=largeLabelFont)
 drawingBoard.grid(row = 2, column = 1)
 
-researchMenu = tkinter.Label(screen, text="Research Menu", font=largeLabelFont)
-researchMenu.grid(row=1, column=2, rowspan=2)
+researchMenu = ResearchMenu(screen)
+researchMenu.grid(row=1, column=2, rowSpan=2, sticky="news")
 
 running = True
 def close():
